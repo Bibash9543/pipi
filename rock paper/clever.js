acdc1dc3199814c5474e67969b4db50de886cee9
Bibash9543 <140886542+Bibash9543@users.noreply.github.com>
2023-10-20 07:00:29 +0400

    Add files via upload

---
+++ rock paper/clever.js
@@ -0,0 +1,6 @@
+var a = 1004567;
+console.time();
+console.warn('This is your last warning');
+console.warn('This is your last warning');
+console.warn('This is your last warning');
+console.timeEnd("Test");
